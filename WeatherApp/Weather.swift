92141cef364f8fb8f798ef1def41c1ddc5087bb6
Charles Graffeo <chgraffeo@gmail.com>
2017-11-10 17:06:51 -0500

    Added weather model

---
+++ WeatherApp/Weather.swift
@@ -0,0 +1,17 @@
+//
+//  Weather.swift
+//  WeatherApp
+//
+//  Created by Charles Graffeo on 11/10/17.
+//  Copyright © 2017 CharlesGraffeo. All rights reserved.
+//
+
+import UIKit
+
+class Weather: NSObject {
+    
+    var main:String?
+    var weatherDescription:String?
+    var icon:String?
+    
+}
