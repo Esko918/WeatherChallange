c2f96a44c9a200195a134f80ca55fdf0947325c4
Charles Graffeo <chgraffeo@gmail.com>
2017-11-10 17:06:38 -0500

    Added city model

---
+++ WeatherApp/City.swift
@@ -0,0 +1,39 @@
+//
+//  City.swift
+//  WeatherApp
+//
+//  Created by Charles Graffeo on 11/10/17.
+//  Copyright © 2017 CharlesGraffeo. All rights reserved.
+//
+
+import ObjectMapper
+
+class City: Mappable {
+    
+    var name:String?
+    var minTemp:Float?
+    var maxTemp:Float?
+    var tempeture:Float?
+    var sunrise:Int?
+    var sunset:Int?
+    var weather:Weather = Weather()
+    
+    required init?(map: Map) {
+        
+    }
+    
+    func mapping(map: Map) {
+        
+        name <- map["name"]
+        minTemp <- map["main.temp_min"]
+        maxTemp <- map["main.temp_max"]
+        tempeture <- map["main.temp"]
+        sunrise <- map["sys.sunrise"]
+        sunset <- map["sys.sunset"]
+        weather.main <- ["weather.main"]
+        weather.weatherDescription <- map["weather.description"]
+        weather.icon <- map["weather.icon"]
+        
+    }
+    
+}
